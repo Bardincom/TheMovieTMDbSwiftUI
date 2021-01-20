7cce83f131a6c52d6bae21ce5acc2733df49ad11
Oleksii Bardin <bardincom@icloud.com>
2021-01-20 17:17:27 +0200

    Main - Start Project

---
+++ TheMovieTMDbSwiftUI/TheMovieTMDbSwiftUI/TheMovieTMDbSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  TheMovieTMDbSwiftUIApp.swift
+//  TheMovieTMDbSwiftUI
+//
+//  Created by Aleksey Bardin on 20.01.2021.
+//
+
+import SwiftUI
+
+@main
+struct TheMovieTMDbSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
