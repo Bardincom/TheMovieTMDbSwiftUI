0baa842b0565f6aeeb244917156febba834c61cd
Oleksii Bardin <bardincom@icloud.com>
2021-01-21 19:48:14 +0200

    Networking - change model MovieInfo

--- TheMovieTMDbSwiftUI/TheMovieTMDbSwiftUI/Model/MovieInfo.swift
+++ TheMovieTMDbSwiftUI/TheMovieTMDbSwiftUI/Model/MovieInfo.swift
@@ -9,8 +9,14 @@ import Foundation
 
 struct MovieInfo: Codable {
     var id: Int
-    var backdrop_path: String
-    var poster_path: String
+    var backdropPath: String
+    var posterPath: String
     var overview: String
     var title: String
+
+    enum CodingKeys: String, CodingKey {
+        case id, overview, title
+        case backdropPath = "backdrop_path"
+        case posterPath = "poster_path"
+    }
 }
