f38833bc6bf273bf4124dc9eefd76dc794b5bbba
Oleksii Bardin <bardincom@icloud.com>
2021-01-24 18:09:39 +0200

    UI - Final changes

--- TheMovieTMDbSwiftUI/TheMovieTMDbSwiftUI/View/LoginScreen/Componets/LoginScreenBackgrountImage.swift
+++ TheMovieTMDbSwiftUI/TheMovieTMDbSwiftUI/View/LoginScreen/Componets/LoginScreenBackgrountImage.swift
@@ -23,3 +23,18 @@ struct LoginScreenBackgroundImage_Previews: PreviewProvider {
         LoginScreenBackgroundImage()
     }
 }
+
+struct LoginScreenBackgroundImage2: View {
+    var body: some View {
+        Image(AssetsImage.loginScreenBackgroundImage)
+            .resizable()
+            .aspectRatio(1/1, contentMode: .fit)
+            .saturation(0.5)
+    }
+}
+
+struct LoginScreenBackgroundImage2_Previews: PreviewProvider {
+    static var previews: some View {
+        LoginScreenBackgroundImage2()
+    }
+}
